c49f272acfdc3fbc257880acb77bb40c66723503
rnppnr <rnprice@hotmail.com>
2023-07-30 19:18:03 +0100

    removed custom css url

--- src/main.ts
+++ src/main.ts
@@ -10,18 +10,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const CSS_URL =
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
+  // const CSS_URL =
+  //   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
+
+  //   const options: SwaggerCustomOptions = {
+  //     customCssUrl: CSS_URL,
+  //   };
 
   const config = new DocumentBuilder()
     .setTitle('Fun Facts API')
     .setDescription('Retrieves fun facts')
     .setVersion('1.0')
     .build();
-
-  const options: SwaggerCustomOptions = {
-    customCssUrl: CSS_URL,
-  };
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, options);
