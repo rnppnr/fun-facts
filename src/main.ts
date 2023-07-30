b4551138c099f6f0dc16e0140f97e8855366c01f
rnppnr <rnprice@hotmail.com>
2023-07-30 17:42:28 +0100

    Swagger tweaks

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,33 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerCustomOptions,
+} from '@nestjs/swagger';
+import swaggerUI from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const CSS_URL =
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
+
   const config = new DocumentBuilder()
     .setTitle('Fun Facts API')
     .setDescription('Retrieves fun facts')
     .setVersion('1.0')
+    .addServer('https://')
     .build();
 
+  const options: SwaggerCustomOptions = {
+    customCssUrl: CSS_URL,
+  };
+
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, options);
 
   await app.listen(3000);
 }
