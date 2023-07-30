ffbcc873e849c65609dcf6761b967e8576f9bf6f
rnppnr <rnprice@hotmail.com>
2023-07-30 19:46:38 +0100

    removed unused import

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  SwaggerModule,
-  DocumentBuilder,
-  SwaggerCustomOptions,
-} from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // const CSS_URL =
-  //   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
-
-  //   const options: SwaggerCustomOptions = {
-  //     customCssUrl: CSS_URL,
-  //   };
 
   const config = new DocumentBuilder()
     .setTitle('Fun Facts API')
@@ -28,4 +17,5 @@ async function bootstrap() {
 
   await app.listen(3000);
 }
+
 bootstrap();
