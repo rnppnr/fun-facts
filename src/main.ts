9c1b9f21a653b8ed63138e5e4880cc08b843b585
rnppnr <rnprice@hotmail.com>
2023-07-30 19:20:21 +0100

    bug fix

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, options);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
